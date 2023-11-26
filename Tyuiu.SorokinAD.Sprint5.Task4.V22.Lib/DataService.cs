9a648b79f99f81f7761df5b5f664be9c73df898c
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-11-26 21:50:43 +0500

    Task4

---
+++ Tyuiu.SorokinAD.Sprint5.Task4.V22.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.SorokinAD.Sprint5.Task4.V22.Lib
+{
+    public class DataService : ISprint5Task4V22
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(Math.Pow(Convert.ToDouble(strX), 3) * Math.Sin(Convert.ToDouble(strX)) - 4 * Convert.ToDouble(strX), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SorokinAD.Sprint5.Task4.V22/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinAD.Sprint5.Task4.V22.Lib;
+using System.IO;
+
+namespace Tyuiu.SorokinAD.Sprint5.Task4.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5| Выполнил: Сорокин А. Д. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #22                                                            *");
+            Console.WriteLine("* Выполнил Сорокин А. Д. | ИИПб-23-2                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму нечетных *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask4V22.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Резльтат                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
